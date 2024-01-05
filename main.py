ae8b7a115e085f5fc886f7ffc6aa06b9c785fed4
BuildTools <unconfigured@null.spigotmc.org>
2024-01-05 19:25:13 +0000

    Attempt to process a file entirely

--- main.py
+++ main.py
@@ -9,7 +9,20 @@ os.chdir(GAME_FILES_PATH+LOC)
 files = os.listdir()
 files = [f for f in files if f[-14:] == ENGLISH] # Filter for english files only
 
+def process_file(lines):
+    """ Take the lines of a file and process them into a dictionary. """
+    loc_map = {}
+    for line in lines:
+        line = line.strip()
+        if line == "l_english:" or line[0] == "#":
+            continue
+
+        key, line = line.split(":")
+        loc = line.split('"')[1]
+        loc_map[key] = loc
+    return loc_map
+
 with open(files[0], encoding="UTF-8-sig") as f:
     lines = f.readlines()
 
-print(lines[:10])
+print(process_file(lines))
